94217dccd2dbfae9e820244264ec5edf4bc15fa3
LHiHLi <54905307+LHiHLi@users.noreply.github.com>
2019-09-19 16:05:37 +0800

    #15 dai

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include"header.h"
+#include"operation.h"
+
+int main()
+{
+//	int count;
+//	count = countingruns();
+
+//	int probnumb;
+//	cout << "请输入题目的数量：";
+//	cin >> probnumb;
+
+	int digits;
+	int number;
+	int i, j = 0, k = 0, l = 0, m;
+
+
+/*	FILE* q;
+	FILE* a;
+	char qfile_name[] = "Q&A_153-155\\Questions_153-155-NN.txt";
+	char afile_name[] = "Q&A_153-155\\Answer_153-155-NN.txt";
+	if (count < 10)
+	{
+		qfile_name[30] = '0';
+		qfile_name[31] = '0' + count;
+		afile_name[27] = '0';
+		afile_name[28] = '0' + count;
+	}
+	else
+	{
+		qfile_name[30] = '0' + count / 10;
+		qfile_name[31] = '0' + count % 10;
+		afile_name[27] = '0' + count / 10;
+		afile_name[28] = '0' + count % 10;
+	}
+	fopen_s(&q, qfile_name, "w");
+	fopen_s(&a, afile_name, "w");
+
+	if (q != NULL)
+		fclose(q);
+	if (a != NULL)
+		fclose(a);
+		*/
+	system("pause");
+	return 0;
+}
